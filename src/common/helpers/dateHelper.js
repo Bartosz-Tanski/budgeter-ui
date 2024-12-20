a0c187be65bcc7e8511d5cb680b7ab82cb956bff
Bartek Tanski <bartitani@gmail.com>
2024-12-20 14:29:37 +0100

    add helpers for dates and transactions

---
+++ src/common/helpers/dateHelper.js
@@ -0,0 +1,11 @@
+﻿const generateDaysInMonth = (year, month) => {
+    const days = [];
+    const date = new Date(year, month, 1);
+    while (date.getMonth() === month) {
+        days.push(new Date(date).toISOString().split("T")[0]); // YYYY-MM-DD
+        date.setDate(date.getDate() + 1);
+    }
+    return days;
+};
+
+export { generateDaysInMonth };
